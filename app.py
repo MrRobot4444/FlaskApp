bf7dfa882cb9336ed4b5ac0c33398ebe9b14cc3a
MrRobot4444 <91490007+MrRobot4444@users.noreply.github.com>
2023-03-11 16:50:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -45,4 +45,4 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(debug=False, host ='0.0.0.0')
+    app.run(debug=True)
